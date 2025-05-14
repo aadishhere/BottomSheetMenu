abb6ae5dee4fe7a5b474da540549f31f79bf8fa5
Aadish Jain <aadishhere@gmail.com>
2025-05-14 15:09:34 +0530

    Initial Commit

---
+++ BottomSheetMenu/BottomSheetMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetMenuApp.swift
+//  BottomSheetMenu
+//
+//  Created by Aadish Jain on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
